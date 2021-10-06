83afe9781b1cfb3343653e1a3e39e01989d97e0e
Fuji <joshua.yocum@northwestern.edu>
2021-10-06 12:50:00 -0500

    setting up rtd for documentation page

---
+++ src/helloWorld.py
@@ -0,0 +1,12 @@
+
+"""This is a simple hello world script"""
+def helloWorld(msg):
+    """
+    This is a simple function that prints a message
+
+    Parameters:
+       msg (str): The string to print
+    """
+    print(msg)
+
+helloWorld('Hello World')
